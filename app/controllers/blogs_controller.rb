e35f7466f357dc919dd177a26d48b267b9d5a44c
Yuta Hoshino <y.hoshino25@gmail.com>
2021-02-09 20:33:08 +0900

    コメントデザイン修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -72,6 +72,7 @@ class BlogsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.find(params[:id])
+      # binding.pry
     end
 
     # Only allow a list of trusted parameters through.
